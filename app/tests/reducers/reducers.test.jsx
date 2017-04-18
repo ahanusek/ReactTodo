84f5274ffe7299d1b2beafe0c22b4cb3a9567b2a
hanys92 <hanusek.a@gmail.com>
2017-04-18 17:40:32 +0200

    Add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,6 +1,7 @@
 var expect = require('expect');
 var reducers = require('reducers')
 var df = require('deep-freeze-strict');
+var moment = require('moment');
 
 describe('Reducers', () => {
 	describe('searchTextReducer', () =>{
@@ -28,4 +29,43 @@ describe('Reducers', () => {
 			expect(res).toEqual(true);
 		});
 	});
+
+	describe('todosReducer', () => {
+		it('should add new todo', () => {
+			var action = {
+				type: "ADD_TODO",
+				text: "Do homework"
+			}
+
+			var res = reducers.todosReducer(df([]), df(action));
+
+			expect(res.length).toEqual(1);
+			expect(res[0].text).toEqual(action.text);
+
+		});
+
+		it('should toggle completed status and toggle timestamp', () => {
+			var action = {
+				type: "TOGGLE_TODO",
+				id: "123"
+			}
+			var todos = [
+				{
+					id: '123',
+					text: "Working",
+					completed: false,
+					date: moment().unix(),
+					completedAt: undefined
+				}
+			]
+
+			var res = reducers.todosReducer(df(todos), df(action));
+
+			expect(res[0].completed).toEqual(true);
+			expect(res[0].completedAt).toNotBeA('undefined');
+
+
+		})
+
+	})
 });
