2a3df37c8712be9622fdae1ff6317f6a2353492c
hanys92 <hanusek.a@gmail.com>
2017-04-18 14:48:39 +0200

    Add redux actions

---
+++ app/actions/actions.jsx
@@ -0,0 +1,32 @@
+export var setSearchText = (searchText) => {
+	return {
+		type: "SET_SEARCH_TEXT",
+		searchText
+	}
+}
+
+//toggleShowCompleted
+export var toggleShowCompleted = () => {
+	return {
+		type: "TOGGLE_SHOW_COMPLETED",
+	}
+}
+
+//toggleTodo(id)
+
+export var toggleTodo = id => {
+	return {
+		type: "TOGGLE_TODO",
+		id
+	}
+}
+
+
+
+
+export var addTodo = (text) => {
+	return {
+		type: "ADD_TODO",
+		text
+	}
+}
